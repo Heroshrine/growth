c0688cee8faa0aec49e809b306a828ad49768652
Skyler McCloud <39038511+Heroshrine@users.noreply.github.com>
2025-05-22 17:22:47 -0700

    Project added from fabric 1.21.1 template project generator & configured (split sources & data gen enabled)

---
+++ src/client/java/heroshrine/growth/GrowthDataGenerator.java
@@ -0,0 +1,11 @@
+package heroshrine.growth;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GrowthDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
